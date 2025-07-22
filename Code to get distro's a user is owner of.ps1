536887410e85077e1ea0c66c662431e6165ca922
goutham-gummadi <goutham.gummadi@lanterncare.com>
2025-07-22 14:31:48 -0500

    Update Code to get distro's a user is owner of.ps1

--- Code to get distro's a user is owner of.ps1
+++ Code to get distro's a user is owner of.ps1
@@ -4,7 +4,7 @@ $groups = Get-DistributionGroup -Filter * | Select-Object Name, ManagedBy
 # Loop through each group
 foreach ($group in $groups) {
     foreach ($owner in $group.ManagedBy) {
-        if ($owner -eq "743b49cd-01ea-460a-93bb-5f92ab26fa46") {
+        if ($owner -eq "") {
             Write-Host "$($group.Name)"
         }
     }
