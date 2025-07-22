23476a796c5d75268b357959241751bc3c290af6
goutham-gummadi <goutham.gummadi@lanterncare.com>
2025-07-22 14:31:31 -0500

    Update Code to get all users under People OU.ps1

--- Code to get all users under People OU.ps1
+++ Code to get all users under People OU.ps1
@@ -2,12 +2,12 @@
 Import-Module ActiveDirectory
 
 # Define the root OU
-$rootOU = "OU=People,DC=corp,DC=edhc,DC=com"
+$rootOU = ""
 $CurrentDate= Get-Date -Format "MM-dd-yyyy"
 
 # Define the CSV file path
-$csvFilePath = "C:\Users\ext.goutham.gummadi\Lantern\Technology - Infrastructure\PhyscialAccessAudit\AD Active Users\UsersList_$CurrentDate.csv"
-$csvFilePath1 = "C:\Users\ext.goutham.gummadi\Lantern\Technology - Infrastructure\AD data match with ADP\AD current users list\UsersList_$CurrentDate.csv"
+$csvFilePath = ""
+$csvFilePath1 = ""
 
 # Retrieve all user accounts from the specified OU and its sub-OUs
 $userAccounts = Get-ADUser -Filter * -SearchBase $rootOU -SearchScope Subtree -Property DisplayName, Title, Department, UserPrincipalName, Info, SamAccountName, EmailAddress, Description, Manager, Company
